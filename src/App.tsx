ce2a20858bd7bfe48f551848eb12efaf8329db1f
Trọng Cảnh <canhtrong26@gmail.com>
2026-08-24 14:56:39 +0700

    Task LT11.40. useEffect + Persist Notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
+
 import AppLayout from './app/AppLayout'
 import NoteList from './components/organisms/NoteList'
 import NoteForm from './components/organisms/NoteForm'
 import mockNotes from './features/notes/mockNotes'
 import type Note from './features/notes/Note'
 import type NoteInput from './features/notes/NoteInput'
+import { useState, useEffect } from 'react'
 
+
+
+const STORAGE_KEY = "noteFlow-notes"
 function App() {
-  const [notes, setNotes] = useState<Note[]>(mockNotes)
-  const [selectedNote, setSelectedNote] = useState<Note | null>(null)
+const [notes, setNotes] = useState<Note[]>(mockNotes)
+const [selectedNote, setSelectedNote] = useState<Note | null>(null)
+const [isLoading, setIsLoading] = useState<boolean>(true)
+const [error, setError] = useState<string | null>(null)
+
+
+useEffect(() => {
+  setIsLoading(true)
+  const stored = localStorage.getItem(STORAGE_KEY)
+  if (stored !== null) {
+    try {
+      setNotes(JSON.parse(stored))
+    } catch {
+      setError("Lỗi đọc dữ liệu!")
+    }
+  }
+  setIsLoading(false)
+}, [])
+
+// useEffect #2
+useEffect(() => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(notes))
+}, [notes])
 
   function handleCreateNote(data: NoteInput) {
     const newNote: Note = {
